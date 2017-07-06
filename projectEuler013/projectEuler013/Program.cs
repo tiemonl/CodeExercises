9c386aed7451763f8ad2bdec553e5c87a61d00a3
tiemonl <tiemon.liam@gmail.com>
2017-07-06 15:36:51 -0400

    added new problem

---
+++ projectEuler013/projectEuler013/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projectEuler013 {
+    class Program {
+        static void Main(string[] args) {
+            Stopwatch s = Stopwatch.StartNew();
+            BigInteger sum=0;
+            char[] nums;
+            StreamReader sr = new StreamReader("..\\..\\num.txt");
+
+            while (sr.Peek() >= 0) {
+                sum += BigInteger.Parse(sr.ReadLine());
+            }
+            nums = sum.ToString().ToCharArray();
+            for (int i = 0; i < 10; i++) {//first ten digits
+                Console.Write(nums[i]);
+            }
+            s.Stop();
+            Console.WriteLine("\nSolution took {0} ms",s.Elapsed.TotalMilliseconds);
+        }
+    }
+}
