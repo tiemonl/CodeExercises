2d3c04fef2f3d221c2a498eb317f0fed32a20a53
tiemonl <tiemon.liam@gmail.com>
2017-07-07 16:22:37 -0400

    added new project

---
+++ projectEuler020/projectEuler020/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projectEuler020 {
+    class Program {
+        static void Main(string[] args) {
+            Stopwatch s = Stopwatch.StartNew();
+            BigInteger num = Factorial(100);
+            int sum = 0;
+            foreach (var item in num.ToString()) {
+                sum += int.Parse(item.ToString());
+            }
+            Console.WriteLine(sum);
+            s.Stop();
+            Console.WriteLine("solution took {0} ms", s.Elapsed.TotalMilliseconds);
+        }
+        private static BigInteger Factorial(int number) {
+            if (number == 0) {
+                return 1;
+            }
+            return number * Factorial(number - 1);
+        }
+    }
+}
