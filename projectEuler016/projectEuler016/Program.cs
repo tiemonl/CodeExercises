a9973e1c486124c67305cb855b777e738451b839
tiemonl <tiemon.liam@gmail.com>
2017-07-07 15:56:30 -0400

    added new project

---
+++ projectEuler016/projectEuler016/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projectEuler016 {
+    class Program {
+        static void Main(string[] args) {
+            //biginteger made this really trivial.
+            Stopwatch s = Stopwatch.StartNew();
+            BigInteger num = BigInteger.Pow(2, 1000);
+            int sum = 0;
+            foreach (var item in num.ToString()) {
+                sum += int.Parse(item.ToString());
+            }
+            Console.WriteLine(sum);
+            s.Stop();
+            Console.WriteLine("soluution took {0} ms", s.Elapsed.TotalMilliseconds);
+        }
+    }
+}
