491a0986b52e5bd2531af20ca3b0643cc9e11ab7
tiemonl <tiemon.liam@gmail.com>
2017-07-07 12:11:09 -0400

    added new project

---
+++ projectEuler008/projectEuler008/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projectEuler008 {
+    class Program {
+        const int SEQUENCE_LENGTH = 13;
+        static void Main(string[] args) {
+            Stopwatch s = Stopwatch.StartNew();
+
+            long total = 1;
+            long highTotal = 1;
+            List<long> nums = new List<long>();
+            StreamReader sr = new StreamReader("..\\..\\num.txt");
+
+            while (sr.Peek() >= 0) {
+                string str = sr.ReadLine();
+                foreach (var item in str.ToCharArray()) {
+                    nums.Add(long.Parse(item.ToString()));
+                }
+            }
+            for (int i = 0; i < nums.Count-SEQUENCE_LENGTH; i++) {
+                total = 1;
+                for (int j = i; j < i+SEQUENCE_LENGTH; j++) {
+                    total *= nums[j];
+                }
+                if (highTotal<total) {
+                    highTotal = total;
+                }
+            }
+            Console.WriteLine(highTotal);
+            s.Stop();
+            Console.WriteLine("solution took {0} ms", s.Elapsed.TotalMilliseconds);
+        }
+    }
+}
---
+++ projectEuler009/projectEuler009/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projectEuler009 {
+    class Program {
+        static void Main(string[] args) {
+            Stopwatch s = Stopwatch.StartNew();
+            double product = 1;
+            for (int a = 1; a < 300; a++) {
+                for (int b = a+1; b < 400; b++) {
+                    //for (int c = b+1; c < 500; c++) { 
+
+                    //getting rid of this loop made the solution over 220 times faster.
+                    //c has to be a double so that it only whole numbers are seen as a valid pythagorean triplet, or else you get a false solution with int truncation.
+                    double c = Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2));
+                        if (/*Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2)) == c && */a+b+c == 1000) {
+                            product *= a * b * c;
+                            break;
+                        } 
+                    //}
+                }
+            }
+            Console.WriteLine(product);
+            s.Stop();
+            Console.WriteLine("solution took: {0} ms", s.Elapsed.TotalMilliseconds);
+        }
+    }
+}
