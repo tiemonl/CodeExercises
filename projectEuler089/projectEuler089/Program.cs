d124ef2b3f12ef9f43dce8c1249153ccd1973f58
tiemonl <tiemon.liam@gmail.com>
2017-07-17 12:59:34 -0400

    added new project

---
+++ projectEuler089/projectEuler089/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projectEuler089 {
+    class Program {
+        static void Main(string[] args) {
+            Stopwatch s = Stopwatch.StartNew();
+            int originalTotal = 0, minTotal = 0;
+            StreamReader sr = new StreamReader("..\\..\\roman.txt");
+
+            while (sr.Peek() >= 0) {
+                string str;
+                str = sr.ReadLine();
+                originalTotal += str.Length;
+                if (str.Contains("VIIII")) {
+                    str = str.Replace("VIIII", "IX");
+                }
+                if (str.Contains("IIII")) {
+                    str = str.Replace("IIII", "IV");
+                }
+                if (str.Contains("LXXXX")) {
+                    str = str.Replace("LXXXX", "XC");
+                }
+                if (str.Contains("XXXX")) {
+                    str = str.Replace("XXXX", "XL");
+                }
+                if (str.Contains("DCCCC")) {
+                    str = str.Replace("DCCCC", "CM");
+                }
+                if (str.Contains("CCCC")) {
+                    str = str.Replace("CCCC", "CD");
+                }
+                minTotal += str.Length;
+            }
+            s.Stop();
+            Console.WriteLine("number of chars saved by writing each number in their minimal form is {0}\nSolution took {1} ms",originalTotal -minTotal,s.Elapsed.TotalMilliseconds);
+        }
+    }
+}
