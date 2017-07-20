445b657e2ed2f3634840c367cc1c36cf9926698c
tiemonl <tiemon.liam@gmail.com>
2017-07-20 17:39:00 -0400

    added new project

--- projectEuler094/projectEuler094/Program.cs
+++ projectEuler094/projectEuler094/Program.cs
@@ -13,29 +13,34 @@ namespace projectEuler094 {
 
             //i first used the commented code below to find almost equilateral triangles, which worked until it lost precision after 10 triangles, so i used those values to find a pattern on https://oeis.org/ to calculate the answer.
             //i also noticed that the third side always rotates on whether to add or subtract from the equal sides;
-            //Stopwatch s = Stopwatch.StartNew();
+            Stopwatch s = Stopwatch.StartNew();
             int[] a = new int[30];
             a[0] = 1;
             a[1] = 1;
             a[2] = 5;
-            for (int i = 3; i < a.Length; i++) {
-                a[i] = 3 * a[i - 1] + 3 * a[i - 2] - a[i - 3];
-            }
+            //for (int i = 3; i < a.Length; i++) {
+            //    a[i] = 3 * a[i - 1] + 3 * a[i - 2] - a[i - 3];
+            //}
             //this loop above calculates the side lengths where we add or subtract 1 to get the perimeter.
             double perimeters = 0;
             double side = 1;
             double m = 1;
             int n = 2;
-            while (perimeters < Math.Pow(10,9)) {
+            while (true) {
                 side = a[n];
                 n++;
+                a[n] = 3 * a[n - 1] + 3 * a[n - 2] - a[n - 3];
                 m = -m;
-                perimeters += (3 * side) - m;
+                if (perimeters + (3 * side) - m < Math.Pow(10,9)) {
+                    perimeters += (3 * side) - m;
+                } else {
+                    break;
+                }
+                
             }
-            perimeters -= (3 * side) - m;
             Console.WriteLine(perimeters);
-            //s.Stop();
-            //Console.WriteLine("Solution took {0} ms", s.Elapsed.TotalMilliseconds);
+            s.Stop();
+            Console.WriteLine("Solution took {0} ms", s.Elapsed.TotalMilliseconds);
 
             //double height;
             //double area;
---
+++ projectEuler142/projectEuler142/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projectEuler142 {
+    class Program {
+        static void Main(string[] args) {
+            /*
+            x+y=A
+            x−y=B
+            x+z=C
+            x−z=D
+            y+z=E
+            y−z=F;
+             
+            x-z = (x+y)-(y+z) // D = A - E
+            x+z = (x+y)-(y−z) // C = A − F
+            x−y = (x+z)-(y+z) // B = C − E
+
+            x−z=D
+            x+z=C
+            x−y=B
+
+            x=D+C/2
+            y=-(2B−D−C)/2
+            z=-(D−C)/2
+            */
+            int x, y, z;
+            
+        }
+    }
+}
